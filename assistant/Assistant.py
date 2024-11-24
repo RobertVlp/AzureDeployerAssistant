a96114af3d209894c23cf1ddb4ddbc02eeb0fd8a
RobertVlp <r.vulpe25@gmail.com>
2024-11-24 16:38:02 +0200

    Initial implementation for Azure storage account, function app and key vault creation functions

---
+++ assistant/Assistant.py
@@ -0,0 +1,113 @@
+from azure.mgmt.storage import StorageManagementClient
+from azure.mgmt.keyvault import KeyVaultManagementClient
+from azure.mgmt.web import WebSiteManagementClient
+
+class Assistant:
+    def __init__(self, credential, subscription_id):
+        self.credential = credential
+        self.subscription_id = subscription_id
+
+    def create_storage_account(self, resource_group_name, storage_account_name, location) -> str:
+        storage_client = StorageManagementClient(self.credential, self.subscription_id)
+
+        storage_async_operation = storage_client.storage_accounts.begin_create(
+            resource_group_name,
+            storage_account_name,
+            {
+                'sku': {'name': 'Standard_LRS'},
+                'kind': 'StorageV2',
+                'location': location
+            }
+        )
+
+        storage_async_operation.result()
+        return f"Storage account {storage_account_name} was created."
+
+    def create_key_vault(self, resource_group_name, key_vault_name, location, access_policies=[]):
+        key_vault_client = KeyVaultManagementClient(self.credential, self.subscription_id)
+        tenantId = "2d8cc8ba-8dda-4334-9e5c-fac2092e9bac" # UPB - Azure for Students
+        
+        key_vault_params = {
+            'location': location,
+            'properties': {
+                'sku': {'family': 'A', 'name': 'standard'},
+                'tenant_id': tenantId,
+                'access_policies': []
+            }
+        }
+
+        for policy in access_policies:
+            key_vault_params["properties"]["access_policies"].append(
+                {
+                    'tenant_id': tenantId,
+                    "object_id": policy["object_id"],
+                    "permissions": policy["permissions"]
+                }
+            )
+
+        key_vault = key_vault_client.vaults.begin_create_or_update(
+            resource_group_name,
+            key_vault_name,
+            key_vault_params
+        ).result()
+
+        return f"Key Vault {key_vault.name} created successfully."
+    
+    def create_function_app(self, resource_group_name, storage_account_name, function_app_name, location):
+        web_client = WebSiteManagementClient(self.credential, self.subscription_id)
+        storage_client = StorageManagementClient(self.credential, self.subscription_id)
+
+        storage_account = storage_client.storage_accounts.get_properties(resource_group_name, storage_account_name)
+
+        # Create an App Service Plan
+        app_service_plan_async_operation = web_client.app_service_plans.begin_create_or_update(
+            resource_group_name,
+            'myAppServicePlan',
+            {
+                'location': location,
+                'sku': {
+                    'name': 'Y1',  # Y1 is the SKU for the Consumption plan
+                    'tier': 'Consumption'
+                },
+                'kind': 'functionapp',
+                'reserved': True
+            }
+        )
+
+        app_service_plan = app_service_plan_async_operation.result()
+
+        print(f"App Service Plan {app_service_plan.name} created successfully.")
+
+        function_app_async_operation = web_client.web_apps.begin_create_or_update(
+            resource_group_name,
+            function_app_name,
+            {
+                'location': location,
+                'server_farm_id': '/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Web/serverfarms/{}'
+                                    .format(self.subscription_id, resource_group_name, app_service_plan.name),
+                'site_config': {
+                    'app_settings': [
+                        {
+                            'name': 'AzureWebJobsStorage', 
+                            'value': 'DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net'
+                                        .format(storage_account.name, storage_account.primary_endpoints.blob)
+                        },
+                        {'name': 'FUNCTIONS_EXTENSION_VERSION', 'value': '~3'},
+                        {'name': 'WEBSITE_RUN_FROM_PACKAGE', 'value': '1'}
+                    ],
+                    'linux_fx_version': 'PYTHON|3.8'
+                },
+                'kind': 'functionapp'
+            }
+        )
+
+        function_app = function_app_async_operation.result()
+
+        print(f"Function App {function_app.name} created successfully.")
+    
+    def get_available_functions(self):
+        return {
+            "create_storage_account": self.create_storage_account,
+            "create_key_vault": self.create_key_vault,
+            "create_function_app": self.create_function_app
+        }
---
+++ deploy-test.py
@@ -0,0 +1,79 @@
+import os
+import json
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
+from typing_extensions import override
+from openai import AssistantEventHandler
+from assistant.Assistant import Assistant
+
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_event(self, event):
+        if event.event == 'thread.run.requires_action':
+            run_id = event.data.id
+            self.handle_requires_action(event.data, run_id)
+        elif event.event == 'thread.message.completed':
+            print(event.data.content[0].text.value)
+ 
+    def handle_requires_action(self, data, run_id):
+        tool_outputs = []
+        
+        for tool in data.required_action.submit_tool_outputs.tool_calls:
+            if tool.function.name not in available_functions:
+                raise Exception("Function requested by the model does not exist.")
+            
+            function_to_call = available_functions[tool.function.name]
+            tool_response = function_to_call(**json.loads(tool.function.arguments))
+            tool_outputs.append({"tool_call_id": tool.id, "output": tool_response})
+        
+        self.submit_tool_outputs(tool_outputs, run_id)
+
+    def submit_tool_outputs(self, tool_outputs, run_id):
+        with client.beta.threads.runs.submit_tool_outputs_stream(
+            thread_id=self.current_run.thread_id,
+            run_id=self.current_run.id,
+            tool_outputs=tool_outputs,
+            event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
+ 
+
+load_dotenv()
+
+subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+assistant_id = os.getenv("ASSISTANT_ID")
+assistant = client.beta.assistants.retrieve(assistant_id)
+
+# Authenticate with Azure
+credential = DefaultAzureCredential()
+
+# Create an assistant
+deployer = Assistant(credential, subscription_id)
+available_functions = deployer.get_available_functions()
+
+# Create a thread
+thread = client.beta.threads.create()
+
+while True:
+    content = input("Enter a message: ")
+    content.rstrip('\n')
+
+    if content == "exit":
+        break
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=content
+    )
+
+    with client.beta.threads.runs.stream(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        event_handler=EventHandler()
+    ) as stream:
+        stream.until_done()
